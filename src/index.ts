a2d57617812e864745baf6568a785fb9b9efd9fe
Vratesh <ghadgevratesh@gmail.com>
2026-03-05 14:53:47 +0530

    reationships in prisma

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,55 @@ const adapter = new PrismaPg({
 });
 export const prisma = new PrismaClient({ adapter });
 
-async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-  const res = await prisma.user.create({
-    data: {
-      username,
-      password,
-      firstName,
-      lastName,
-    },
-  });
-  console.log(res);
+// async function insertUser(username: string, password: string, firstName: string, lastName: string) {
+//   const res = await prisma.user.create({
+//     data: {
+//       username,
+//       password,
+//       firstName,
+//       lastName,
+//     },
+//   });
+//   console.log(res);
+// }
+
+// insertUser("exampl1212e1", "password11223", "John11212", "Doe11212")
+
+
+// async function getTodoAndUserDetails(userId: number) {
+//     await prisma.todo.create({
+//         data: {
+//             title: "Todo 1",
+//             descriptin: "This is the first todo",
+//             userId: 1,
+//         },
+//     });
+// }
+
+async function getTodoAndUserDetails(userId: number) {
+
+    // await prisma.todo.create({
+    //     data: {
+    //         title: "Todo 1",
+    //         descriptin: "This is the first todo",
+    //         userId: 1,
+    //     },
+    // }); 
+    
+
+    const res = await prisma.todo.findMany({
+        //kind of JOINS i.e we are fetching the user details along with the todo details
+        where: {
+            userId,
+        },
+        select: {
+            id: true,
+            title: true,
+            descriptin: true,
+            user: true
+        }
+    });
+    console.log(res);
 }
 
-insertUser("exampl1212e1", "password11223", "John11212", "Doe11212")
+getTodoAndUserDetails(1);
