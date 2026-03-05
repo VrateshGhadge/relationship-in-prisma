b2d360cc370e197abcf0cbbc3cbfa13b6465a624
Vratesh <ghadgevratesh@gmail.com>
2026-03-05 13:11:04 +0530

    reationships in prisma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -7,5 +8,15 @@ const adapter = new PrismaPg({
 export const prisma = new PrismaClient({ adapter });
 
 async function insertUser(username: string, password: string, firstName: string, lastName: string) {
-  
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstName,
+      lastName,
+    },
+  });
+  console.log(res);
 }
+
+insertUser("exampl1212e1", "password11223", "John11212", "Doe11212")
